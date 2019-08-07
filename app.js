72cd7f936fc657ffaeea1601da252bf1a5bf7417
SnowSurfer <drinkaziu@gmail.com>
2019-08-07 16:28:54 -0400

    toggle startGame

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
+  },
+  methods: {
+    startGame() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack() {
+      let attackBlow = Math.floor(Math.random() * 6);
+      this.monsterHealth -= attackBlow;
+    }
+  }
 })
