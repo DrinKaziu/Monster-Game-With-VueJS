438ade0c44aa1f6e0f7993dc5ee7476a62616d96
SnowSurfer <drinkaziu@gmail.com>
2019-08-07 17:04:25 -0400

    implement attacks

--- app.js
+++ app.js
@@ -11,9 +11,51 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+    calculateBlow(minBlow, maxBlow) {
+      return Math.max(Math.floor(Math.random() * maxBlow) + 1, minBlow);
+    },
+    monsterAttack() {
+      this.playerHealth -= this.calculateBlow(5, 12);
+    },
     attack() {
-      let attackBlow = Math.floor(Math.random() * 6);
-      this.monsterHealth -= attackBlow;
+      this.monsterHealth -= this.calculateBlow(3, 10);
+      if(this.checkWin()) {
+        return 
+      }
+      this.monsterAttack()
+      this.checkWin()
+    },
+    specialAttack() {
+      this.monsterHealth -= this.calculateBlow(10, 20);
+      if(this.checkWin()) {
+        return 
+      }
+      this.monsterAttack()
+      this.checkWin()
+    }, 
+    heal() {
+
+    }, 
+    giveUp() {
+
+    },
+    checkWin() {
+      if(this.monsterHealth <= 0) {
+        if(confirm("You won!! New Game?")) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if(this.playerHealth <= 0) {
+        if(confirm("You Lost!! New Game?")) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true; 
+      }
+      return false
     }
   }
 })
